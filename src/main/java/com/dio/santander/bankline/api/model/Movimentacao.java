10b70444b25d8c45805a2828aba941102ccbbd24
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-15 20:53:39 -0300

    feat: mapping entities with JPA

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -1,12 +1,26 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tab_movimentacao")
 public class Movimentacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "data_hora")
     private LocalDateTime dataHora;
+
     private String descricao;
+
     private Double valor;
+
+    @Column(name = "id_conta")
+    private Integer idConta;
+
+    @Enumerated(EnumType.STRING)
     private MovimentacaoTipo tipo;
 
     public Integer getId() {
@@ -38,5 +52,11 @@ public class Movimentacao {
     }
     public void setTipo(MovimentacaoTipo tipo) {
         this.tipo = tipo;
+    }
+    public Integer getIdConta() {
+        return idConta;
+    }
+    public void setIdConta(Integer idConta) {
+        this.idConta = idConta;
     }
 }
