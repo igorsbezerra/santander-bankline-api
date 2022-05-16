e05bbf747dfb95d75ab311a4ac535a52f55a21ed
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-16 13:00:19 -0300

    feat: format json return column 'dataHora'

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -1,5 +1,7 @@
 package com.dio.santander.bankline.api.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
@@ -11,6 +13,7 @@ public class Movimentacao {
     private Integer id;
 
     @Column(name = "data_hora")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dataHora;
 
     private String descricao;
